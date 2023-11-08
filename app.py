144b1335247b0850c51fed7efcbda5a4f51ad5f5
kyj0107 <karla0107@aol.com>
2023-11-08 12:00:10 -0600

    Added query to obtain posts and modified index.html to display posts

--- app.py
+++ app.py
@@ -9,15 +9,34 @@ app.config["DEBUG"] = True
 #flash  the secret key to secure sessions
 app.config['SECRET_KEY'] = 'your secret key'
 
+#function to open a connection to the database.db file
+def get_db_connection():
+    #get a database connection
+    conn = sqlite3.connect('database.db')
 
+    #allows name-based access to columns
+    #rows from db connection can be used like python dictionaries
+    conn.row_factory = sqlite3.Row
 
+    #return connection object
+    return conn
 
 # use the app.route() decorator to create a Flask view function called index()
 @app.route('/')
 def index():
-    
-    
-    return "<h1>Index Page<\h1>"
+    #get a database connection
+    conn = get_db_connection()
+
+    #execute a query to get all posts from the database
+    #use fetchall() to get all the rows (remember the final project from 3380?)
+
+    query = 'SELECT * FROM posts'
+    posts = conn.execute(query).fetchall()
+
+    #close the connectioin
+    conn.close()
+
+    return render_template('index.html', posts=posts)
 
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
